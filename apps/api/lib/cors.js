0f6e7b70ae1ffcb348c2f7bb3996eb532c79a956
Tu Nombre <hola@solonet.es>
2025-09-08 17:34:08 +0200

    corrigiendo cors en el buscador

--- apps/api/lib/cors.js
+++ apps/api/lib/cors.js
@@ -2,10 +2,26 @@
 
 import Cors from 'cors';
 
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'https://buscador.afland.es',
+    'https://nuevobuscador.afland.es',
+];
+
 // Inicializamos el middleware de CORS
 const cors = Cors({
     methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'], // Permite estos métodos
-    origin: 'http://127.0.0.1:5500', // El origen de tu frontend
+    origin: (origin, callback) => {
+        // Permite solicitudes sin origen (como aplicaciones móviles o solicitudes curl)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin) || /https:\/\/.*-angel-picon-caleros-projects\.vercel\.app$/.test(origin)) {
+            return callback(null, true);
+        }
+
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+    },
 });
 
 // Helper para ejecutar el middleware
