954a3c429333f90acdb7055a414a668a71338b99
Tu Nombre <hola@solonet.es>
2025-09-30 20:22:36 +0200

    nuevas correcciones en paylo9ad quitando sharp

--- apps/payload-cms/src/payload.config.ts
+++ apps/payload-cms/src/payload.config.ts
@@ -5,7 +5,6 @@ import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
-import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -30,7 +29,7 @@ export default buildConfig({
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
   }),
-  sharp,
+
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
