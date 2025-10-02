e4f64bdd99f66219e3f68abdafa93259eeb85c98
Tu Nombre <hola@solonet.es>
2025-10-02 17:17:29 +0200

    volviendoa ver los errores types

--- apps/payload-cms/src/collections/Artists.ts
+++ apps/payload-cms/src/collections/Artists.ts
@@ -3,15 +3,15 @@
 import type { CollectionConfig } from 'payload/types'
 
 const slugify = (text: string): string => {
-    return text
-        .toString()
-        .toLowerCase()
-        .trim()
-        .normalize('NFD') // Normaliza para separar acentos de las letras
-        .replace(/[\u0300-\u036f]/g, '') // Elimina los diacríticos (acentos)
-        .replace(/\s+/g, '-') // Reemplaza espacios con guiones
-        .replace(/[^\w\-]+/g, '') // Elimina todos los caracteres no alfanuméricos excepto guiones
-        .replace(/\-\-+/g, '-'); // Reemplaza múltiples guiones con uno solo
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .normalize('NFD') // Normaliza para separar acentos de las letras
+    .replace(/[\u0300-\u036f]/g, '') // Elimina los diacríticos (acentos)
+    .replace(/\s+/g, '-') // Reemplaza espacios con guiones
+    .replace(/[^\w\-]+/g, '') // Elimina todos los caracteres no alfanuméricos excepto guiones
+    .replace(/\-\-+/g, '-'); // Reemplaza múltiples guiones con uno solo
 };
 
 export const Artists: CollectionConfig = {
